b0257898705342990ef83d4c99f323c25bf7ee99
Chris Smith <chris@chameth.com>
2018-10-18 19:46:29 +0100

    Handle extended joins

--- src/main/kotlin/com/dmdirc/ktirc/model/CapabilitiesState.kt
+++ src/main/kotlin/com/dmdirc/ktirc/model/CapabilitiesState.kt
@@ -17,23 +17,24 @@ enum class CapabilitiesNegotiationState {
 
 }
 
+@Suppress("unused")
 sealed class Capability(val name: String) {
     // Capabilities that enable more information in message tags:
-    object ServerTimeMessageTag : Capability("server-time") // TODO: Parse this and expose time in events
-    object UserAccountMessageTag : Capability("account-tag") // TODO: Add accounts to user info
+    object ServerTimeMessageTag : Capability("server-time")
+    object UserAccountMessageTag : Capability("account-tag")
 
     // Capabilities that extend existing commands to supply extra information:
     object HostsInNamesReply : Capability("userhost-in-names")
     object MultipleUserModePrefixes : Capability("multi-prefix")
-    object AccountAndRealNameInJoinMessages : Capability("extended-join") // TODO: Parse this
+    object AccountAndRealNameInJoinMessages : Capability("extended-join")
 
     // Capabilities that affect how messages are sent/received:
     object EchoMessages : Capability("echo-message")
 
     // Capabilities that notify us of changes to other clients:
-    object AccountChangeMessages : Capability("account-notify")
-    object AwayStateMessages : Capability("away-notify")
-    object HostChangeMessages : Capability("chghost")
+    object AccountChangeMessages : Capability("account-notify") // TODO: Add processor
+    object AwayStateMessages : Capability("away-notify") // TODO: Add processor
+    object HostChangeMessages : Capability("chghost") // TODO: Add processor
 }
 
 val capabilities: Map<String, Capability> by lazy {
--- src/test/kotlin/com/dmdirc/ktirc/messages/JoinProcessorTest.kt
+++ src/test/kotlin/com/dmdirc/ktirc/messages/JoinProcessorTest.kt
@@ -32,4 +32,26 @@ internal class JoinProcessorTest {
         assertEquals(0, events.size)
     }
 
+    @Test
+    fun `JoinProcessor adds real name and account from extended join`() {
+        val events = JoinProcessor().process(
+                IrcMessage(emptyMap(), "acidburn!libby@root.localhost".toByteArray(), "JOIN", listOf("#crashandburn", "acidBurn", "Libby").map { it.toByteArray() }))
+        assertEquals(1, events.size)
+
+        assertEquals(TestConstants.time, events[0].time)
+        assertEquals(User("acidburn", "libby", "root.localhost", account = "acidBurn", realName = "Libby"), events[0].user)
+        assertEquals("#crashandburn", events[0].channel)
+    }
+
+    @Test
+    fun `JoinProcessor ignores account if the user is not authed`() {
+        val events = JoinProcessor().process(
+                IrcMessage(emptyMap(), "acidburn!libby@root.localhost".toByteArray(), "JOIN", listOf("#crashandburn", "*", "Libby").map { it.toByteArray() }))
+        assertEquals(1, events.size)
+
+        assertEquals(TestConstants.time, events[0].time)
+        assertEquals(User("acidburn", "libby", "root.localhost", realName = "Libby"), events[0].user)
+        assertEquals("#crashandburn", events[0].channel)
+    }
+
 }
