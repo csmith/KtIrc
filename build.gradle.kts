f91426f72763bcc552758fbbc2801f96082fea42
Chris Smith <chris@chameth.com>
2019-01-31 21:09:06 +0000

    First pass at publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+version = "0.1.0"
+group = "com.dmdirc.ktirc"
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.3.0-rc-190"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 configurations {
@@ -40,10 +45,16 @@ java {
     }
 }
 
-tasks.create<Test>("itest") {
+task<Test>("itest") {
     group = "verification"
     testClassesDirs = java.sourceSets.getByName("itest").output.classesDirs
     classpath = java.sourceSets.getByName("itest").runtimeClasspath
+}
+
+task<Jar>("sourceJar") {
+    description = "Creates a JAR that contains the source code."
+    from(java.sourceSets["main"].allSource)
+    classifier = "sources"
 }
 
 tasks.withType<KotlinCompile> {
@@ -64,5 +75,32 @@ configurations.all {
         if (requested.group == "org.jetbrains.kotlin") {
             useVersion("1.3.0-rc-190")
         }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("Publication") {
+            groupId = "com.dmdirc"
+            artifactId = "ktirc"
+            version = project.version as String
+            artifact(tasks["jar"])
+            artifact(tasks["sourceJar"])
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+    setPublications("Publication")
+    with(pkg) {
+        userOrg = "dmdirc"
+        repo = "releases"
+        name = "ktirc"
+        publish = true
+        desc = "A kotlin library for connecting to and interacting with IRC"
+        setLicenses("MIT")
+        vcsUrl = "https://g.c5h.io/public/ktirc"
     }
 }
