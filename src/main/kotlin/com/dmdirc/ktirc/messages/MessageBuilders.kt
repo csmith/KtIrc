90167d4cb0da97e579a608ecc8120b57e8e9384e
Chris Smith <chris@chameth.com>
2019-02-04 15:57:10 +0000

    Support sending CTCPs and actions

--- src/main/kotlin/com/dmdirc/ktirc/messages/MessageBuilders.kt
+++ src/main/kotlin/com/dmdirc/ktirc/messages/MessageBuilders.kt
@@ -4,19 +4,34 @@ import com.dmdirc.ktirc.IrcClient
 
 /** Sends a message to ask the server to list capabilities. */
 internal fun IrcClient.sendCapabilityList() = send("CAP LS 302")
+
 /** Sends a message indicating the end of capability negotiation. */
 internal fun IrcClient.sendCapabilityEnd() = send("CAP END")
+
 /** Sends a message requesting the specified caps are enabled. */
 internal fun IrcClient.sendCapabilityRequest(capabilities: List<String>) = send("CAP REQ :${capabilities.joinToString(" ")}")
+
 /** Sends a request to join the given channel. */
 fun IrcClient.sendJoin(channel: String) = send("JOIN :$channel")
+
 /** Sends a request to change to the given nickname. */
 fun IrcClient.sendNickChange(nick: String) = send("NICK :$nick")
+
 /** Sends the connection password to the server. */
 internal fun IrcClient.sendPassword(password: String) = send("PASS :$password")
+
 /** Sends a response to a PING event. */
 internal fun IrcClient.sendPong(nonce: ByteArray) = send("PONG :${String(nonce)}")
+
+/** Sends a CTCP message of the specified [type] and with optional [data] to [target] (a user or a channel). */
+fun IrcClient.sendCtcp(target: String, type: String, data: String? = null) =
+        sendMessage(target, "\u0001${type.toUpperCase()}${data?.let { " $it" } ?: ""}\u0001")
+
+/** Sends an action to the given [target] (a user or a channel). */
+fun IrcClient.sendAction(target: String, action: String) = sendCtcp(target, "ACTION", action)
+
 /** Sends a private message to a user or channel. */
 fun IrcClient.sendMessage(target: String, message: String) = send("PRIVMSG $target :$message")
+
 /** Sends a message to register a user with the server. */
 internal fun IrcClient.sendUser(userName: String, localHostName: String, serverHostName: String, realName: String) = send("USER $userName $localHostName $serverHostName :$realName")
--- src/test/kotlin/com/dmdirc/ktirc/messages/MessageBuildersTest.kt
+++ src/test/kotlin/com/dmdirc/ktirc/messages/MessageBuildersTest.kt
@@ -10,49 +10,67 @@ internal class MessageBuildersTest {
     private val mockClient = mock<IrcClient>()
 
     @Test
-    fun `CapabilityRequestMessage creates CAP REQ message with single argument`() {
+    fun `sendCapabilityRequest sends CAP REQ message with single argument`() {
         mockClient.sendCapabilityRequest(listOf("a"))
         verify(mockClient).send("CAP REQ :a")
     }
 
     @Test
-    fun `CapabilityRequestMessage creates CAP REQ message with multiple args`() {
+    fun `sendCapabilityRequest sends CAP REQ message with multiple args`() {
         mockClient.sendCapabilityRequest(listOf("a b c"))
         verify(mockClient).send("CAP REQ :a b c")
     }
 
     @Test
-    fun `JoinMessage creates correct JOIN message`() {
+    fun `sendJoin sends correct JOIN message`() {
         mockClient.sendJoin("#TheGibson")
         verify(mockClient).send("JOIN :#TheGibson")
     }
 
     @Test
-    fun `NickMessage creates correct NICK message`() {
+    fun `sendNickChange sends correct NICK message`() {
         mockClient.sendNickChange("AcidBurn")
         verify(mockClient).send("NICK :AcidBurn")
     }
 
     @Test
-    fun `PasswordMessage creates correct PASS message`() {
+    fun `sendPassword sends correct PASS message`() {
         mockClient.sendPassword("hacktheplanet")
         verify(mockClient).send("PASS :hacktheplanet")
     }
 
     @Test
-    fun `PongMessage creates correct PONG message`() {
+    fun `sendPong sends correct PONG message`() {
         mockClient.sendPong("abcdef".toByteArray())
         verify(mockClient).send("PONG :abcdef")
     }
 
     @Test
-    fun `PrivmsgMessage creates correct PRIVMSG message`() {
+    fun `sendMessage sends correct PRIVMSG message`() {
         mockClient.sendMessage("acidBurn", "Hack the planet!")
         verify(mockClient).send("PRIVMSG acidBurn :Hack the planet!")
     }
 
     @Test
-    fun `UserMessage creates correct USER message`() {
+    fun `sendCtcp sends correct CTCP message with no arguments`() {
+        mockClient.sendCtcp("acidBurn", "ping")
+        verify(mockClient).send("PRIVMSG acidBurn :\u0001PING\u0001")
+    }
+
+    @Test
+    fun `sendCtcp sends correct CTCP message with arguments`() {
+        mockClient.sendCtcp("acidBurn", "ping", "12345")
+        verify(mockClient).send("PRIVMSG acidBurn :\u0001PING 12345\u0001")
+    }
+
+    @Test
+    fun `sendAction sends correct action`() {
+        mockClient.sendAction("acidBurn", "hacks the planet")
+        verify(mockClient).send("PRIVMSG acidBurn :\u0001ACTION hacks the planet\u0001")
+    }
+
+    @Test
+    fun `sendUser sends correct USER message`() {
         mockClient.sendUser("AcidBurn", "localhost", "gibson", "Kate")
         verify(mockClient).send("USER AcidBurn localhost gibson :Kate")
     }
